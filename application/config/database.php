60c043405d53b6a891d4078d692edd5d7c1a62c7
Aldo Rey Balagulan <aldo.balagulan@gmail.com>
2014-02-04 00:45:12 +0800

    Initial work on SQLite db
    - Added an SQLite source file that automatically initializes an empty SQLite
    database
    - Modified necessary settings in the database configs

--- application/config/database.php
+++ application/config/database.php
@@ -48,11 +48,11 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
+$db['default']['hostname'] = 'sqlite:'.APPPATH.'db/shounenwatch.sqlite';
 $db['default']['username'] = '';
 $db['default']['password'] = '';
 $db['default']['database'] = '';
-$db['default']['dbdriver'] = 'mysql';
+$db['default']['dbdriver'] = 'pdo'; # pdo driver for sqlite3
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
---
+++ application/db/init_db.sql
@@ -0,0 +1,63 @@
+/*
+    init_db.sql
+    
+    Run this .sql file whenever you are in a new environment with no sqlite3
+    database initialized.
+    
+    It will create the necessary tables, as well as create the initial
+    superuser.
+    
+    Note that we are lib
+*/
+
+-- Create the user table
+CREATE TABLE user (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    username VARCHAR(30)  NOT NULL,
+    -- password set to 48 chars, 40 needed by SHA1, 8 chars reserved for salt
+    password TEXT NOT NULL,
+    image TEXT,
+    about TEXT
+);
+
+-- Create the anime table
+CREATE TABLE anime (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    userID INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    synopsis TEXT NOT NULL,
+    episodes INTEGER NOT NULL,
+    image VARCHAR(80),
+    airing DATE NOT NULL,
+    FOREIGN KEY (userID) REFERENCES user(id)
+);
+
+-- Create the rating table
+CREATE TABLE rating (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    userID INTEGER NOT NULL,
+    animeID INTEGER NOT NULL,
+    rating INTEGER NOT NULL,
+    FOREIGN KEY (userID) REFERENCES user(id),
+    FOREIGN KEY (animeID) REFERENCES anime(id)
+);
+
+-- Create the watching table
+CREATE TABLE watching (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    userID INTEGER NOT NULL,
+    animeID INTEGER NOT NULL,
+    currentEpisode INTEGER NOT NULL,
+    FOREIGN KEY (userID) REFERENCES user(id),
+    FOREIGN KEY (animeID) REFERENCES anime(id)
+);
+
+-- Populate the user table with the superuser
+INSERT INTO user (
+    username, password, about
+)
+VALUES (
+    "admin",
+    "bd85dd49d4cb9736bcc084c784701a807832952b14819bbd96dd5ccb6b3578bf",
+    "I'm the number one badass around here!"
+);
