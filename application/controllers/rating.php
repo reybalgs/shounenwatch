ddd8a4198557d64b2e971d865cf890fb3823d081
Aldo Rey Balagulan <aldo.balagulan@gmail.com>
2014-02-17 13:28:47 +0800

    Add more functions for ratings
    - Can now retrieve all ratings from a user
    - Can now retrieve all ratings for an anime
    - Can now retrieve a user's rating for an anime
    - Can now calculate the average of ratings, useful for displaying rating stars.

--- application/controllers/rating.php
+++ application/controllers/rating.php
@@ -5,8 +5,29 @@ class Rating extends CI_Controller {
         $this->load->model('rating_model');
     }
     
+    public function get_rating_average($ratings) {
+        # Returns the average (on a scale of .5s) of the given array of ratings.
+        $ratings_count = count($ratings);
+        $total_ratings = 0;
+        
+        # Add all the ratings together
+        foreach($ratings as $rating) {
+            $total_ratings += $rating['rating'];
+        }
+        
+        # Get the average
+        $total_ratings = $total_ratings / ($ratings_count * 1.0);
+        
+        # Round by the nearest .5
+        return round($total_ratings, 1);
+    }
+    
     public function index() {
         $data['ratings'] = $this->rating_model->get_all_ratings();
+        $data['reluctanthero'] = $this->rating_model->get_all_ratings_from_anime(17);
+        $data['rhaverage'] = $this->get_rating_average($this->rating_model->get_all_ratings_from_anime(17));
+        $data['phoenixwright'] = $this->rating_model->get_all_ratings_from_user(5);
+        $data['pwaarating'] = $this->rating_model->get_user_anime_rating(5, 1);
         $data['title'] = 'Rating test';
         
         $this->load->view('templates/header', $data);
--- application/models/rating_model.php
+++ application/models/rating_model.php
@@ -14,6 +14,43 @@ class Rating_model extends CI_Model {
         $query = $this->db->get();
         return $query->result_array();
     }
+    
+    public function get_all_ratings_from_user($user_id) {
+        # Gets all ratings from the given user.
+        $this->db->select('user.id as userID, user.username, anime.id as animeID, anime.name, rating.id as ratingID, rating.rating');
+        $this->db->from('anime');
+        $this->db->join('rating', 'anime.id = rating.animeID');
+        $this->db->join('user', 'rating.userID = user.id');
+        $this->db->where('rating.userID', $user_id);
+        
+        $query = $this->db->get();
+        return $query->result_array();
+    }
+    
+    public function get_all_ratings_from_anime($anime_id) {
+        # Gets all ratings on the given anime.
+        $this->db->select('user.id as userID, user.username, anime.id as animeID, anime.name, rating.id as ratingID, rating.rating');
+        $this->db->from('anime');
+        $this->db->join('rating', 'anime.id = rating.animeID');
+        $this->db->join('user', 'rating.userID = user.id');
+        $this->db->where('rating.animeID', $anime_id);
+        
+        $query = $this->db->get();
+        return $query->result_array();
+    }
+    
+    public function get_user_anime_rating($user_id, $anime_id) {
+        # Gets the given user's rating on the given anime.
+        $this->db->select('user.id as userID, user.username, anime.id as animeID, anime.name, rating.id as ratingID, rating.rating');
+        $this->db->from('anime');
+        $this->db->join('rating', 'anime.id = rating.animeID');
+        $this->db->join('user', 'rating.userID = user.id');
+        $this->db->where('rating.animeID', $anime_id);
+        $this->db->where('rating.userID', $user_id);
+        
+        $query = $this->db->get();
+        return $query->row();
+    }
 }
 
 ?>
