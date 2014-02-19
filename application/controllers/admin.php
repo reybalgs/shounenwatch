c45ecc2cce188026c79ce67868e8928d0a674ce9
Aldo Rey Balagulan <aldo.balagulan@gmail.com>
2014-02-19 13:03:59 +0800

    Add basic lists to the admin panel
    - Admin panel should now see a list of all users, anime, and reports
    - Will work on functions like deleting and such later
    - Thinking of adding pagination for the tables once things get too big

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -4,10 +4,14 @@ class Admin extends CI_Controller {
         parent::__construct();
         $this->load->model('user_model');
         $this->load->model('anime_model');
+        $this->load->model('reports_model');
     }
     
     public function index() {
         $data['title'] = 'ShounenWatch Admin Panel';
+        $data['users'] = $this->user_model->get_users();
+        $data['anime'] = $this->anime_model->get_all_anime();
+        $data['reports'] = $this->reports_model->get_all_reports();
         
         $this->load->view('templates/header', $data);
         $this->load->view('admin/panel', $data);
